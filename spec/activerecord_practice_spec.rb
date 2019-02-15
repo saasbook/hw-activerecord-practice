ff91b7a9b5bc7183b6bd4f21a79ba8ec953861b1
Armando Fox <fox@cs.berkeley.edu>
2019-02-15 15:28:23 -0800

    set default formatter for rspec, and use transactions for database-writing tests

--- spec/activerecord_practice_spec.rb
+++ spec/activerecord_practice_spec.rb
@@ -1,9 +1,35 @@
 require 'rspec'
 require './activerecord_practice.rb'
 
-describe 'customers' do
-  specify 'with invalid email' do
-    result = Customer.with_invalid_email
-    expect(result.size).to eq(10)
+RSpec.configure do |config|
+  config.default_formatter = 'doc'
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
   end
 end
+
+include Enumerable
+module Enumerable
+  def to_simple_list
+    self.map { |c| c.to_a rescue c }.flatten.sort
+  end
+end
+
+describe 'selecting customers:' do
+  specify 'with valid email' do
+    result = Customer.with_valid_email
+    expect(result.size).to eq(10)
+  end
+  specify 'born before 1/1/1980'
+  specify 'with valid email AND born before 1/1/1980'
+  specify '20 youngest customers (hint: lookup ActiveRelation `order` and `limit`)'
+end
+
+describe 'modifying customers:' do
+  specify 'change invalid emails to be blank'
+end
+
+
